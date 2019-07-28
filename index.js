fb8bb03d3a4ad575ee26b9251c90d61b1a47ab7e
avi <anatoly.ivanov@gmail.com>
2019-07-28 10:53:10 -0700

    refactoring using async/await

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-var rp = require('request-promise-native');
-var genieURI = 'https://genie.exosite.com/api/portals/v1/';
-var genieRPC_URI = 'https://genie.m2.exosite.com/onep:v1/rpc/process';
-var genieRPC_Auth;
-var genieRPC_Header;
-var genieAppHeader = {
-  'AppVersion': '3.0.0',
-  'BundleName': 'com.geniecompany.AladdinConnect',
-  'User-Agent': 'Aladdin Connect iOS v3.0.0',
-  'BuildVersion': '131',
+const rp = require('request-promise-native');
+const genieURI = 'https://genie.exosite.com/api/portals/v1/';
+const genieRPC_URI = 'https://genie.m2.exosite.com/onep:v1/rpc/process';
+const genieAppHeader = {
+    'AppVersion': '3.0.0',
+    'BundleName': 'com.geniecompany.AladdinConnect',
+    'User-Agent': 'Aladdin Connect iOS v3.0.0',
+    'BuildVersion': '131',
 }
-var genieTokenHeader;
-var userId;
-var portalId;
-var portalDetails;
+
+const DEBUG = 1;
+
+function debug(info, obj) {
+  if (DEBUG) {
+    console.log('[*] DEBUG: ' + info + ': ' + JSON.stringify(obj));
+  }
+}
 
 function getDoorState(statusNumber) {
   switch(statusNumber) {
@@ -29,121 +31,147 @@ function getDoorState(statusNumber) {
     case 6: // Timeout Closing
       return 'CLOSING,'
     case 7: // Not Configured
+    default: // Error
       return 'STOPPED'
   }
 }
 
-module.exports = function(user, password, action, callback, deviceNumber = 0, doorNumber = 1) {
+async function openClose(shouldOpen, genieRPC_URI,genieRPC_Header,genieRPC_Auth,doorNumber, callback) {
+   try {
+    await rp({
+                method: 'POST',
+                uri: genieRPC_URI,
+                headers: genieRPC_Header,
+                body: {
+                  auth: genieRPC_Auth,
+                  calls: [
+                    {
+                      arguments: [ { alias: 'dps' + doorNumber + '.desired_status' }, shouldOpen ],
+                      id: 1,
+                      procedure: 'write'
+                    },
+                    {
+                      arguments: [ { alias: 'dps' + doorNumber + '.desired_status_user' }, user ],
+                      id: 1,
+                      procedure: 'write'
+                    },
+                  ]
+                },
+                json: true,
+              });
+      return callback(shouldOpen?'OPEENING':'CLOSING');
+    } catch (error) {
+      return callback('STOPPED'); 
+    }
+}
+
+async function getStatus(genieRPC_URI,genieRPC_Header,genieRPC_Auth,doorNumber, callback) {
+  try {
+    let response = await rp({
+      method: 'POST',
+      uri: genieRPC_URI,
+      headers: genieRPC_Header,
+      body: {
+        auth: genieRPC_Auth,
+        calls: [
+          {
+            arguments: [ { alias: 'dps' + doorNumber + '.door_status' }, {} ],
+            id: 1,
+            procedure: 'read'
+          },
+        ]
+      },
+      json: true,
+    });
+    
+    debug('door_status response', response);
+
+    callback(getDoorState(response ? response[0].result[0][1] : -1 )); 
+
+  } catch (error) {
+    console.log(error); 
+  }
+}
+
+module.exports = async (user, password, action, callback, deviceNumber = 0, doorNumber = 1) => {
+  var genieRPC_Auth;
+  var genieRPC_Header;
+  var genieTokenHeader;
+  var userId;
+  var portalId;
+  var portalDetails;
+  
   // 1: get loginToken
-  rp({
+  let loginToken = await rp({
     method: 'GET',
     uri: genieURI + 'users/_this/token',
     headers: Object.assign({}, genieAppHeader, {
       'Authorization': 'Basic '+ Buffer.from(user + ':' + password).toString('base64'),
     }),
     json: true,
-  }).then(function (response) {
-    var loginToken = response;
-    genieTokenHeader = Object.assign({}, genieAppHeader, { 'Authorization': 'Token: ' + loginToken });
-    // 2: get userID
-    rp({ method: 'GET', uri: genieURI + 'users/_this', headers: genieTokenHeader, json: true, })
-    .then(function(response) {
-      userId = response.id;
-      // 3: get portalId
-      rp({ method: 'GET', uri: genieURI + 'users/' + userId + '/portals', headers: genieTokenHeader, json: true, })
-      .then(function(response) {
-        portalId = response[0].PortalID;
-        // 4: get portalDetails
-        rp({ method: 'GET', uri: genieURI + 'portals/' + portalId, headers: genieTokenHeader, json: true })
-        .then(function(response) {
-          portalDetails = response;
-          genieRPC_Auth = {
-            cik: portalDetails.info.key,
-            client_id: portalDetails.devices[deviceNumber]
-          }
-          genieRPC_Header = Object.assign({}, genieAppHeader, {
-            'Authorization': 'Token: ' + loginToken,
-            'Content-Type': 'application/json',
-          });
-          switch(action) {
-            case 'open':
-              rp({
-                method: 'POST',
-                uri: genieRPC_URI,
-                headers: genieRPC_Header,
-                body: {
-                  auth: genieRPC_Auth,
-                  calls: [
-                    {
-                      arguments: [ { alias: 'dps' + doorNumber + '.desired_status' }, 1 ],
-                      id: 1,
-                      procedure: 'write'
-                    },
-                    {
-                      arguments: [ { alias: 'dps' + doorNumber + '.desired_status_user' }, user ],
-                      id: 1,
-                      procedure: 'write'
-                    },
-                  ]
-                },
-                json: true,
-              })
-              .then(function(response) { return callback('OPENING'); })
-              .catch(function(error) { return callback('STOPPED'); });
-              break;
-            case 'close':
-              rp({
-                method: 'POST',
-                uri: genieRPC_URI,
-                headers: genieRPC_Header,
-                body: {
-                  auth: genieRPC_Auth,
-                  calls: [
-                    {
-                      arguments: [ { alias: 'dps' + doorNumber + '.desired_status' }, 0 ],
-                      id: 1,
-                      procedure: 'write'
-                    },
-                    {
-                      arguments: [ { alias: 'dps' + doorNumber + '.desired_status_user' }, user ],
-                      id: 1,
-                      procedure: 'write'
-                    },
-                  ]
-                },
-                json: true,
-              })
-              .then(function(response) { return callback('CLOSING'); })
-              .catch(function(error) { return callback('STOPPED') });
-              break;
-            case 'status':
-            default:
-              rp({
-                method: 'POST',
-                uri: genieRPC_URI,
-                headers: Object.assign({}, genieAppHeader, {
-                  'Authorization': 'Token: ' + loginToken,
-                  'Content-Type': 'application/json',
-                }),
-                body: {
-                  auth: genieRPC_Auth,
-                  calls: [
-                    {
-                      arguments: [ { alias: 'dps' + doorNumber + '.door_status' }, {} ],
-                      id: 1,
-                      procedure: 'read'
-                    },
-                  ]
-                },
-                json: true,
-              })
-              .then(function(response) {  callback(getDoorState(response[0].result[0][1])); })
-              .catch(function(error) { console.log(error); });
-            }
-        });
-      });
-    });
-  }).catch(function(error) {
-    console.log(error);
   });
-};
+  
+  debug('Token',loginToken);
+  
+  let genieTokenHeader = Object.assign({}, genieAppHeader, { 'Authorization': 'Token: ' + loginToken });
+  
+  // 2: get userID
+  let responseUser = await rp({ 
+    method: 'GET', 
+    uri: genieURI + 'users/_this', 
+    headers: genieTokenHeader, 
+    json: true, 
+  });
+
+  debug('users/_this',responseUser);
+  
+  let userId = responseUser.id;
+
+  // 3: get portalId
+  let portals = await rp({ 
+    method: 'GET', 
+    uri: genieURI + 'users/' + userId + '/portals', 
+    headers: genieTokenHeader, 
+    json: true, 
+  });
+
+  debug('Portals', portals);
+
+  let portalId = portals[0].PortalID;
+  
+  // 4: get portalDetails
+  let portalDetails = await rp({
+    method: 'GET', 
+    uri: genieURI + 'portals/' + portalId, 
+    headers: genieTokenHeader, 
+    json: true 
+  });
+
+  debug('Portals/'+portalId, portalDetails);
+  
+  // use info.key as a part of auth token
+  let genieRPC_Auth = {
+    cik: portalDetails.info.key,
+    client_id: portalDetails.devices[deviceNumber]
+  };
+
+  debug('PortalDetails.Info',portalDetails.info);
+
+  let genieRPC_Header = Object.assign({}, genieAppHeader, {
+    'Authorization': 'Token: ' + loginToken,
+    'Content-Type': 'application/json',
+  });
+
+  switch(action) {
+    case 'open':
+      await openClose(1, genieRPC_URI,genieRPC_Header,genieRPC_Auth,doorNumber, callback);
+      break;
+    case 'close':
+      await openClose(0, genieRPC_URI,genieRPC_Header,genieRPC_Auth,doorNumber, callback);
+      break;
+    case 'status':
+    default:
+      await getStatus(genieRPC_URI,genieRPC_Header,genieRPC_Auth,doorNumber, callback);
+  }
+}
+
