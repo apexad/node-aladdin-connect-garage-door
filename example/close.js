7a91d8160c56b8e092e73c813262e177c808e2b2
apexad <apexad@icloud.com>
2020-01-23 23:08:16 -0700

    chore(cleanup): remove example, usage in README should be enough

--- example/close.js
+++
@@ -1,7 +0,0 @@
-var aladdinGarageDoor = require('node-aladdin-connect-garage-door');
-
-function callback(text)  {
-  console.log(text);
-}
-
-aladdinGarageDoor('USER', 'PASSWORD', 'close', callback, 0, 1);
--- example/open.js
+++
@@ -1,7 +0,0 @@
-var aladdinGarageDoor = require('node-aladdin-connect-garage-door');
-
-function callback(text)  {
-  console.log(text);
-}
-
-aladdinGarageDoor('USER', 'PASSWORD', 'open', callback, 0, 1);
